e43c61d68588c631163df0edadd0ffd32a1dafb9
phrl42 <phrl42@riseup.net>
2023-11-05 15:22:03 +0100

    working on opcode implementation generation

--- Sandbox/src/gbc/cpu.cpp
+++ Sandbox/src/gbc/cpu.cpp
@@ -1,16 +1,17 @@
 #include "gbc/cpu.h"
-
 #include "gbc/bitwise.h"
-#include "gbc/opfunction.h"
 
 #include "Application.hpp"
+
+#define Z_FLAG 7
+#define N_FLAG 6
+#define H_FLAG 5
+#define C_FLAG 4
 
 namespace GBC
 {
   void CPU::Validate_Opcode(Bus *bus)
   {
-    bus->begin = Banana::Application::GetInstance().GetWindow().GetTime();
-    
     uint8_t opcode = bus->Read(PC);
    // keep in mind: the GameBoy CPU (SM83) has Little-Endianness (reads multiple bytes backwards from ram)
     /*switch(opcode)
@@ -26,24 +27,24 @@ namespace GBC
     case 0x01:
     {
       spec->PC++;
-      LD16(&spec->BC, Combine(spec->ram[spec->PC + 1], spec->ram[spec->PC]));
+      LD16(&BC, Combine(bus->Read(PC + 1), bus->Read(PC)));
       break;
     }
     
     // LD [BC], A
     case 0x02:
     {
-      uint8_t register_a = (spec->AF >> 4);
+      uint8_t register_a = (AF >> 4);
       
-      spec->PC += 2;
-      spec->ram[spec->BC] = register_a;
+      PC += 2;
+      bus->Write(BC, register_a);
       break;
     }
 
     // INC BC
     case 0x03:
     {
-      INC16(&spec->BC);
+      INC16(&BC);
       break;
     }
 
@@ -2129,4 +2130,476 @@ namespace GBC
     spec->PC++;
     */
   }
+
+     // starts counting from LSB
+  void CPU::Set_Bit_N(uint16_t *reg, uint8_t n, uint8_t val)
+  {
+    // todo: assert
+    if(val > 1)
+    {
+      GBC_LOG("Cannot set bit. Val > 1");
+      return;
+    }
+    if(n > 16)
+    {
+      GBC_LOG("Could not set bit. n > 16");
+      return;
+    }
+
+    uint16_t n_val = *reg;
+    n_val <<= 15 - n;
+    n_val >>= 15;
+
+    if(val == n_val)
+    {
+      return;
+    }
+    n_val = val;
+    uint16_t s_reg = n_val;
+    s_reg <<= n;
+    s_reg |= *reg;
+
+    *reg = s_reg;
+
+    return;
+  }
+
+  void CPU::Set_Half_Carry(uint16_t *flags_register, uint16_t src_register, uint16_t val, bool bit8)
+  {
+    uint16_t erase_val = 0x0F;
+    uint16_t check_val = 0x10;
+    if(!bit8)
+    {
+      erase_val = 0x0F00;
+      check_val = 0x1000;
+    }
+
+    src_register &= erase_val;
+    val &= erase_val;
+    
+    src_register += val;
+    if((src_register & check_val) == check_val)
+    {
+      Set_Bit_N(flags_register, H_FLAG, 1);
+    }      
+    return;
+  }
+
+  void CPU::Set_Carry_Plus(uint16_t *flags_register, uint16_t src_register, uint16_t val, bool bit8)
+  {
+    uint16_t check_val = 0xFF;
+    if(!bit8)
+    {
+      check_val = 0xFFFF;
+    }
+
+    if((int)(src_register + val) > check_val)
+    {
+      Set_Bit_N(flags_register, C_FLAG, 1);
+    }
+
+  }
+
+  void CPU::Set_Carry_Minus(uint16_t *flags_register, uint16_t src_register, uint16_t val)
+  {
+    if((int)(src_register - val) < 0)
+    {
+      Set_Bit_N(flags_register, C_FLAG, 1);
+    }
+  }
+  
+  uint16_t CPU::Get_Bit_N(uint16_t src, uint8_t n)
+  {
+    if(n > 16)
+    {
+      GBC_LOG("Could not get bit. n > 16");
+      return 0;
+    }
+
+    src <<= n - 15;
+    src >>= 15;
+
+    return src;
+  }
+
+  // Exceptions are in the switch statement. Such as: 0x08 (LD [a16], SP)
+  // Relative Jumps are also there
+
+  // LoaD 8-Bit value from RAM into Register
+  void CPU::LD8(uint16_t *dest_register, uint8_t src_value, bool higher_half)
+  {
+    uint16_t reg = *dest_register;
+    if(higher_half)
+    {
+      uint8_t l = reg;
+      reg = src_value << 8 | l;
+    }
+    else
+    {
+      uint8_t h = reg >> 8;
+      reg = h << 8 | src_value;
+    }
+    *dest_register = reg;
+    return;
+  }
+
+  // LoaD 16-Bit value from 
+  void CPU::LD16(uint16_t *dest_register, uint16_t src_value)
+  {
+    *dest_register = src_value;
+    return;
+  }
+
+  // Increment 8-Bit Register
+  void CPU::INC8(uint16_t *flags_register, uint16_t *dest_register, bool higher_half)
+  {
+    uint16_t reg = *dest_register;
+    if(higher_half)
+    {
+      uint8_t h = reg >> 8;
+
+      Set_Half_Carry(flags_register, h, 1, true);
+      h++;
+
+      if(h == 0)
+      {
+	Set_Bit_N(flags_register, Z_FLAG, 1);
+      }
+      uint8_t l = reg;
+      reg = (h << 8) | l;
+    }
+    else
+    {
+      uint8_t h = reg >> 8;
+      uint8_t l = reg;
+      l++;
+      if(l == 0)
+      {
+	Set_Bit_N(flags_register, Z_FLAG, 1);
+      }
+      reg = (h << 8) | l;
+    }
+
+    Set_Bit_N(flags_register, N_FLAG, 0);
+    *dest_register = reg;
+    return;
+  }
+  
+  // Increment 16-Bit Register
+  void CPU::INC16(uint16_t *dest_register)
+  {
+    *dest_register++;
+    return;
+  }
+
+  
+  // Decrement 8-Bit Register
+  void CPU::DEC8(uint16_t *flags_register, uint16_t *dest_register, bool higher_half)
+  {
+    uint16_t reg = *dest_register;
+    if(higher_half)
+    {
+      uint8_t h = reg >> 8;
+
+      Set_Half_Carry(flags_register, h, -1, true);
+
+      h--;
+      if(!h)
+      {
+	Set_Bit_N(flags_register, Z_FLAG, 1);
+      }
+      uint8_t l = reg;
+      reg = (h << 8) | l;
+    }
+    else
+    {
+      uint8_t h = reg >> 8;
+      uint8_t l = reg;
+
+      Set_Half_Carry(flags_register, l, -1, true);
+
+      l--;
+      if(!l)
+      {
+	Set_Bit_N(flags_register, Z_FLAG, 1);
+      }
+      reg = (h << 8) | l;
+    }
+
+    Set_Bit_N(flags_register, N_FLAG, 1);
+
+    *dest_register = reg;
+    return;
+  }
+
+  // Decrement 16-Bit Register
+  void CPU::DEC16(uint16_t *dest_register)
+  {
+    *dest_register--;
+    return;
+  }
+
+  // ADD 8-Bit value to Register
+  void CPU::ADD8(uint16_t *flags_register, uint16_t *dest_register, uint8_t src_value, bool higher_half)
+  {
+    uint16_t reg = *dest_register;
+    if(higher_half)
+    {
+      uint8_t h = reg >> 8;
+      uint8_t l = reg;
+
+      Set_Carry_Plus(flags_register, h, src_value, true);
+      Set_Half_Carry(flags_register, h, src_value, true);
+      
+      h += src_value;
+      if(!h)
+      {
+	Set_Bit_N(flags_register, Z_FLAG, 1);
+      }
+      reg = h << 8 | l;
+    }
+    else
+    {
+      uint8_t h = reg >> 8;
+      uint8_t l = reg;
+
+      Set_Carry_Plus(flags_register, l, src_value, true);
+      Set_Half_Carry(flags_register, l, src_value, true);
+      l += src_value;
+
+      if(!l)
+      {
+	Set_Bit_N(flags_register, Z_FLAG, 1);
+      }
+      reg = (h << 8) | l;
+    }
+    *dest_register = reg;
+
+    Set_Bit_N(flags_register, N_FLAG, 0);
+    return;
+  }
+
+  // ADD 16-Bit value to Register
+  void CPU::ADD16(uint16_t *flags_register, uint16_t *dest_register, uint16_t src_value)
+  {
+    Set_Half_Carry(flags_register, *dest_register, src_value, false);
+    Set_Carry_Plus(flags_register, *dest_register, src_value, false);
+
+    *dest_register += src_value;
+ 
+    Set_Bit_N(flags_register, N_FLAG, 0);
+    return;
+  }
+
+  // SUB 8-Bit value from Register
+  void CPU::SUB8(uint16_t *flags_register, uint16_t *dest_register, uint8_t src_value, bool higher_half)
+  {
+    uint16_t reg = *dest_register;
+    if(higher_half)
+    {
+      uint8_t h = reg >> 8;
+      uint8_t l = reg;
+
+      Set_Half_Carry(flags_register, h, -1 * src_value, true);
+
+      Set_Carry_Minus(flags_register, h, src_value);
+      h -= src_value;
+      if(!h)
+      {
+	Set_Bit_N(flags_register, Z_FLAG, 1);
+      }
+      
+     reg = h << 8 | l;
+    }
+    else
+    {
+      uint8_t h = reg >> 8;
+      uint8_t l = reg;
+
+      Set_Half_Carry(flags_register, l, -1 * src_value, true);
+      
+      Set_Carry_Minus(flags_register, l, src_value);
+
+      l -= src_value;
+
+      if(!l)
+      {
+	Set_Bit_N(flags_register, Z_FLAG, 1);
+      }
+
+      reg = (h << 8) | l;
+    }
+    *dest_register = reg;
+
+    Set_Bit_N(flags_register, N_FLAG, 1);
+    return;
+  }
+
+  
+  // These ones only modify Register 'A' so I could remove the general part
+  // Bitwise AND Register with value
+  void CPU::AND8(uint16_t *flags_register, uint16_t *dest_register, uint8_t src_value, bool higher_half)
+  {
+    uint16_t reg = *dest_register;
+    if(higher_half)
+    {
+      uint8_t h = reg >> 8;
+      uint8_t l = reg;
+
+      h &= src_value;
+      if(!h)
+      {
+	Set_Bit_N(flags_register, Z_FLAG, 1);
+      }
+      reg = h << 8 | l;
+    }
+    else
+    {
+      uint8_t h = reg >> 8;
+      uint8_t l = reg;
+
+      l &= src_value;
+      if(!l)
+      {
+	Set_Bit_N(flags_register, Z_FLAG, 1);
+      }
+      reg = (h << 8) | l;
+    }
+    *dest_register = reg;
+
+    Set_Bit_N(flags_register, N_FLAG, 0);
+    Set_Bit_N(flags_register, H_FLAG, 1);
+    Set_Bit_N(flags_register, C_FLAG, 0);
+    return;
+  }
+
+  // Bitwise OR on Register with value
+  void CPU::OR8(uint16_t *flags_register, uint16_t *dest_register, uint8_t src_value, bool higher_half)
+  {
+    uint16_t reg = *dest_register;
+    if(higher_half)
+    {
+      uint8_t h = reg >> 8;
+      uint8_t l = reg;
+
+      h |= src_value;
+      if(!h)
+      {
+	Set_Bit_N(flags_register, Z_FLAG, 1);
+      }
+      reg = h << 8 | l;
+    }
+    else
+    {
+      uint8_t h = reg >> 8;
+      uint8_t l = reg;
+
+      l |= src_value;
+      if(!l)
+      {
+	Set_Bit_N(flags_register, Z_FLAG, 1);
+      }
+      reg = (h << 8) | l;
+    }
+    *dest_register = reg;
+
+    Set_Bit_N(flags_register, N_FLAG, 0);
+    Set_Bit_N(flags_register, H_FLAG, 0);
+    Set_Bit_N(flags_register, C_FLAG, 0);
+    return;
+  }
+
+  // Bitwise XOR on Register with value
+  // keep exception at 0xAF in mind
+  void CPU::XOR8(uint16_t *flags_register, uint16_t *dest_register, uint8_t src_value, bool higher_half)
+  {
+    uint16_t reg = *dest_register;
+    if(higher_half)
+    {
+      uint8_t h = reg >> 8;
+      uint8_t l = reg;
+
+      h ^= src_value;
+      if(!h)
+      {
+	Set_Bit_N(flags_register, Z_FLAG, 1);
+      }
+      reg = h << 8 | l;
+    }
+    else
+    {
+      uint8_t h = reg >> 8;
+      uint8_t l = reg;
+
+      l ^= src_value;
+      if(!l)
+      {
+	Set_Bit_N(flags_register, Z_FLAG, 1);
+      }
+      reg = (h << 8) | l;
+    }
+    *dest_register = reg;
+
+    Set_Bit_N(flags_register, N_FLAG, 0);
+    Set_Bit_N(flags_register, H_FLAG, 0);
+    Set_Bit_N(flags_register, C_FLAG, 0);
+  }
+
+  
+  // Custom operation on Register with 8-Bit value
+  // keep exception at 0x9F in mind
+  void CPU::SBC8(uint16_t *flags_register, uint16_t *dest_register, uint8_t src_value, bool higher_half)
+  {
+    uint16_t val = src_value - Get_Bit_N(*flags_register, 4);
+    Set_Half_Carry(flags_register, *dest_register, -1 * val, true);
+    Set_Carry_Minus(flags_register, *dest_register, val);
+    
+    *dest_register -= val;
+
+    if(*dest_register == 0)
+    {
+      Set_Bit_N(flags_register, Z_FLAG, 1);
+    }
+    
+    Set_Bit_N(flags_register, N_FLAG, 1);
+  }
+
+  // Custom operation on Register with 8-Bit value
+  void CPU::ADC8(uint16_t *flags_register, uint16_t *dest_register, uint8_t src_value, bool higher_half)
+  {
+    uint16_t val = src_value + Get_Bit_N(*flags_register, C_FLAG);
+    
+    Set_Half_Carry(flags_register, *dest_register, val, true);
+    Set_Carry_Plus(flags_register, *dest_register, val, true);
+    *dest_register += val;
+
+    if(*dest_register == 0)
+    {
+      Set_Bit_N(flags_register, Z_FLAG, 1);
+    }
+     
+    Set_Bit_N(flags_register, N_FLAG, 0);
+  }
+
+  // Custom operation on Register with 8-Bit value
+  // keep exception at 0xBF in mind
+  
+  void CPU::CP8(uint16_t *flags_register, uint16_t *dest_register, uint8_t src_value, bool higher_half)
+  {
+    uint16_t cc = *flags_register >> 8;
+    uint16_t sub = src_value + Get_Bit_N(*flags_register, C_FLAG);
+
+    Set_Half_Carry(flags_register, cc, -1 * sub, true);
+    Set_Carry_Minus(flags_register, cc, sub);
+    cc -= src_value;
+    
+    if(!cc)
+    {
+      Set_Bit_N(flags_register, Z_FLAG, 1);
+    }
+    Set_Bit_N(flags_register, N_FLAG, 1);
+  }
+
+ 
+
 };
--- Sandbox/src/gbc/cpu.h
+++ Sandbox/src/gbc/cpu.h
@@ -7,23 +7,80 @@ namespace GBC
 
   struct CPU
   {
-    // Registers
-    uint8_t A = 0;
-    uint8_t F = 0; // Flags
-    
-    uint8_t B = 0;
-    uint8_t C = 0;
+    uint16_t AF = 0; // Accumulator and Flags
 
-    uint8_t D = 0;
-    uint8_t E = 0;
+    // switchable registers
+    uint16_t BC = 0;
+    uint16_t DE = 0;
+    uint16_t HL = 0;
 
-    uint8_t H = 0;
-    uint8_t L = 0;
-    
     uint16_t SP = 0; // Stack Pointer
     uint16_t PC = 0; // Program Counter
  
     void Validate_Opcode(Bus *bus);
+
+  private:
+    void Set_Bit_N(uint16_t *reg, uint8_t n, uint8_t val);
+    uint16_t Get_Bit_N(uint16_t src, uint8_t n);
+  
+    void Set_Half_Carry(uint16_t *flags_register, uint16_t src_register, uint16_t val, bool bit8);
+    void Set_Carry_Plus(uint16_t *flags_register, uint16_t src_register, uint16_t val, bool bit8);
+    void Set_Carry_Minus(uint16_t *flags_register, uint16_t src_register, uint16_t val);
+  
+    // Exceptions are in the switch statement. Such as: 0x08 (LD [a16], SP)
+    // Relative Jumps are also there
+    // LoaD 8-Bit value from RAM into Register
+    void LD8(uint16_t *dest_register, uint8_t src_value, bool higher_half);
+
+    // LoaD 16-Bit value from 
+    void LD16(uint16_t *dest_register, uint16_t src_value);
+
+    // Increment 8-Bit Register
+    void INC8(uint16_t *flags_register, uint16_t *dest_register, bool higher_half);
+  
+    // Increment 16-Bit Register
+    void INC16(uint16_t *dest_register);
+
+  
+    // Decrement 8-Bit Register
+    void DEC8(uint16_t *flags_register, uint16_t *dest_register, bool higher_half);
+
+    // Decrement 16-Bit Register
+    void DEC16(uint16_t *dest_register);
+
+  
+    // ADD 8-Bit value to Register
+    void ADD8(uint16_t *flags_register, uint16_t *dest_register, uint8_t src_value, bool higher_half);
+
+    // ADD 16-Bit value to Register
+    void ADD16(uint16_t *flags_register, uint16_t *dest_register, uint16_t src_value);
+
+    // SUB 8-Bit value from Register
+    void SUB8(uint16_t *flags_register, uint16_t *dest_register, uint8_t src_value, bool higher_half);
+
+  
+    // These ones only modify Register 'A' so I could remove the general part
+    // Bitwise AND Register with value
+    void AND8(uint16_t *flags_register, uint16_t *dest_register, uint8_t src_value, bool higher_half);
+
+    // Bitwise OR on Register with value
+    void OR8(uint16_t *flags_register, uint16_t *dest_register, uint8_t src_value, bool higher_half);
+
+    // Bitwise XOR on Register with value
+    // keep exception at 0xAF in mind
+    void XOR8(uint16_t *flags_register, uint16_t *dest_register, uint8_t src_value, bool higher_half);
+
+  
+    // Custom operation on Register with 8-Bit value
+    // keep exception at 0x9F in mind
+    void SBC8(uint16_t *flags_register, uint16_t *dest_register, uint8_t src_value, bool higher_half);
+
+    // Custom operation on Register with 8-Bit value
+    void ADC8(uint16_t *flags_register, uint16_t *dest_register, uint8_t src_value, bool higher_half);
+
+    // Custom operation on Register with 8-Bit value
+    // keep exception at 0xBF in mind
+    void CP8(uint16_t *flags_register, uint16_t *dest_register, uint8_t src_value, bool higher_half);
   };
   
   static inline const uint8_t opcycle[256] =
